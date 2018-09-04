8c54b521406b635afba73934889d65f7cf068f8c
Maxim Shaptala <maxshaptala@gmail.com>
2018-09-04 17:20:12 +0300

    Тест дб

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-
 <h1>Hello <?= isset($_GET['name']) ? $_GET['name'] : "Guests" ?></h1>
 
 <p>Connection string<?= $_ENV['MYSQLCONNSTR_localdb'] ?></p>
+
+<?php
+try {
+    $db = new PDO("mysql:host=localhost", "azure", "");
+} catch (PDOException $ex) {
+    echo $ex->getMessage();
+}
+
 
