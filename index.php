3e68df61ad96385ee14fcb86968f105644da2d85
Maxim Shaptala <maxshaptala@gmail.com>
2018-09-04 17:22:29 +0300

    Тест дб

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <h1>Hello <?= isset($_GET['name']) ? $_GET['name'] : "Guests" ?></h1>
 
-<p>Connection string<?= $_ENV['MYSQLCONNSTR_localdb'] ?></p>
+<p>Connection string<?= $_SERVER['MYSQLCONNSTR_localdb'] ?></p>
 
 <?php
 try {
-    $db = new PDO("mysql:host=localhost", "azure", "");
+    $db = new PDO("mysql:host=localhost;port=49175", "azure", "");
 } catch (PDOException $ex) {
     echo $ex->getMessage();
 }
